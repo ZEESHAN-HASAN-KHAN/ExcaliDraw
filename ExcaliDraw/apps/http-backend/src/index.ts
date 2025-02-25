b0aa1e59827a30200ec9b2aeb1204276d5e8f216
ZEESHAN HASAN KHAN <1906525@kiit.ac.in>
2025-02-25 19:30:28 +0530

    httpendpoint

--- ExcaliDraw/apps/http-backend/src/index.ts
+++ ExcaliDraw/apps/http-backend/src/index.ts
@@ -2,44 +2,101 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import {JWT_SECRET} from "@repo/backend-common/config"
 import { middleware } from "./middleware";
-import {CreateUserSchema,SiginSchema,CreateRoomSchema} from "@repo/common/types"
+import { CreateUserSchema, SiginSchema, CreateRoomSchema } from "@repo/common/types"
+import { prismaClient } from "@repo/db/client";
 const app = express();
-app.post("/signup", (req, res) => {
+app.use(express.json());
+app.post("/signup", async(req, res) => {
     //db call
-    const data = CreateUserSchema.safeParse(req.body);
-    if (!data.success)
+    const parsedData = CreateUserSchema.safeParse(req.body);
+    if (!parsedData.success)
     {
          res.json({
             message:"InCorrect inputs"
          })
         return;
     }
-    res.json({
-        userId:"124"
-    })
+    try {
+      const user=  await prismaClient.user.create({
+            data: {
+              email: parsedData.data?.username,
+                //TODO Hash the password
+                password: parsedData.data.password,
+                name:     parsedData.data.name,
+            }
+        })
+        res.json({
+            userId:user.id
+        })
+        
+    } catch (e)
+    {
+        res.status(411).json({
+            message:"User Already Exists with this username"
+        })
+    }
 })
-app.post("/signin", (req, res) => {
-    const data = SiginSchema.safeParse(req.body);
-    if (!data.success)
+app.post("/signin", async(req, res) => {
+    const parsedData = SiginSchema.safeParse(req.body);
+    if (!parsedData.success)
     {
          res.json({
             message:"InCorrect inputs"
          })
         return;
     }
-    const userId = 1;
+    //TODO comapare the hash password here
+    const user = await prismaClient.user.findFirst({
+        where: {
+            email: parsedData.data.username,
+            password:parsedData.data.password
+        }
+    })
+    if (!user)
+    {
+        res.status(403).json({
+            message:"No Author"
+        })
+        return;
+       }
     const token = jwt.sign({
-      userId
+      userId:user?.id
     }, JWT_SECRET)
     res.json({
-        token
-    })
+        token    })
 })
 
-app.post("/room", middleware, (req, res) => {
-    res.json({
-        roomId:123
-    })
+app.post("/room", middleware, async(req, res) => {
+    const parsedData = CreateRoomSchema.safeParse(req.body);
+    if (!parsedData.success)
+    {
+        res.json({
+            message:"Incorrect Inputs"
+        })
+        return;
+    }
+    //db call
+    //@ts-ignore
+    const userId = req.userId;
+    
+    try {
+       
+        const room = await prismaClient.room.create({
+            data: {
+                slug: parsedData.data.name,
+                adminId:userId
+            }
+        })
+        res.json({
+            roomId:room.id
+        })
+    }
+    catch (e)
+    {
+        res.status(411).json({
+            message:"Room Already Exist  with this name"
+        })
+    }
 })
  
 app.listen(3001);
--- ExcaliDraw/apps/http-backend/src/middleware.ts
+++ ExcaliDraw/apps/http-backend/src/middleware.ts
@@ -3,13 +3,17 @@ import { JWT_SECRET } from "@repo/backend-common/config";
 import jwt from "jsonwebtoken"
 export function middleware(req: Request, res: Response, next: NextFunction)
 {
+
+    const token = req.headers['authorization'];
     //@ts-ignore
-    const token = req.headers("authorization");
     const decoded = jwt.verify(token, JWT_SECRET);
     //@ts-ignore
     if (decoded.userId)
     {
-
+       //@ts-ignore
+        req.userId = decoded.userId
+        next();
+        
     }
     else
     {
